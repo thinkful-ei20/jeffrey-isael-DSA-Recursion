7fb0677259743c96cb01ea5e9412011b8824011f
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-06-18 12:43:49 -0600

    finished binaryRepresentation

---
+++ binaryRepresentation.js
@@ -0,0 +1,24 @@
+'use strict';
+/*
+    input number ex = 3
+    output = 11
+    if 0 should return 0
+    number / 2 = 1 | number % 2 = 1
+    number / 2 = 0 | number % 2 = 1
+*/
+
+function binaryRepresentation(num){
+  if(num === 0){ // if num is === 0
+    return '';
+  }
+
+  if(isNaN(num/num)){
+    return 0; // if the number is zero 
+  }
+    
+  let binary = Math.floor(num%2); // binary string
+
+  return binaryRepresentation(Math.floor(num/2)) + binary;// return binary
+}
+
+console.log(binaryRepresentation(25));
