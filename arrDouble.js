7a0e2c96d15d017bdd9758164846e3009c3ade06
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-06-18 10:49:16 -0500

    Array Double

--- arrDouble.js
+++ arrDouble.js
@@ -1,7 +1,9 @@
-function arrDouble(arr){
-    if(!arr.length) return [];
+'use strict';
 
-    return [arr[0] * 2, ...arrDouble(arr.slice(1))];
+function arrDouble(arr) {
+  if (!arr.length) return [];
+
+  return [arr[0] * 2, ...arrDouble(arr.slice(1))];
 }
 
-console.log(arrDouble([1,2,3, 5]));
+console.log(arrDouble([1, 2, 3, 5])); // eslint-disable-line no-console
