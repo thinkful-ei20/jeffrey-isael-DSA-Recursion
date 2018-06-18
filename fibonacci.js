5d9a4ce2169a2dc61223892fe24b9b527e1d74a9
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-06-18 14:52:37 -0500

    Fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+  input: number e.g. 7
+  output: 13
+  return 0 if num is 0
+  return 1 if num <= 2
+  return fib(num-1) + fib(num-2)
+*/
+
+function fib(num) {
+  if (num < 1) {
+    return 0;
+  }
+
+  if (num <= 2) {
+    return 1;
+  }
+
+  return fib(num - 1) + fib(num - 2);
+}
+
+console.log(fib(7));
+// TODO: PRINT SEQUENCE INSTEAD OF JUST NUMBER
